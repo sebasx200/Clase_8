240e1d5a551982ac16e1bb0e20fcf8f9217c9332
Sebastian Palacio <sebasx200@hotmail.com>
2023-05-01 13:49:04 -0500

    Crea listas con ejemplos e imprime su tamaño, elementos y suma entre elementos. Adicional crea el .gitignore

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+edad = 22
+altura = 1.83
+nombre = "Sebastián"
+estado = True
+
+lista_1 = [18, 22, 23, 12]
+lista_2 = [1.83, 1.50, 1.66, 1.77]
+lista_3 = ["Lunes", "Martes", "Miércoles"]
+lista_4 = ["Sebastián", "22", 1.83, True]
+if __name__ == '__main__':
+
+    print(len(lista_1))
+    print(len(lista_2))
+    print(len(lista_3))
+    print(len(lista_4))
+    print()
+    print (lista_1)
+    lista_1[0] = 20
+    print(lista_1)
+    print(lista_1[0] + lista_1[3])
